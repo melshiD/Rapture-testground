7c0a5d668238a308d007e05b4f2e407ae0200542
David Melsheimer <dave.melshman@gmail.com>
2021-10-11 14:10:50 -0400

    experimenting with generating a tooltip of an entire die

--- rapture-die-load-test.js
+++ rapture-die-load-test.js
@@ -11,44 +11,47 @@
 
 //To access the 'use' elements inside of the svg and die divs
 //firstDie.firstElementChild.children[1].attributes[0].value = "#triangle-top";
+var color = "gold";
 
-var dieFaceArray = [
-                ["red", "gold", "four", "circle", "7"],
+var dieFacesArray = [
+                ["red", color, "four", "circle", "7"],
                 ["red", "gold", "one", "square", "7"],
                 ["white", "red", "two", "square", "7"],
-                ["white", "red", "none", "triangle", "7"],
+                ["white", "gold", "three", "triangle", "7"],
                 ["red", "gold", "three", "hexagon", "7"],
-                ["white", "red", "five", "triangle", "7"]
-            ];//just one face, not a whole bag yet!
+                ["white", color, "five", "triangle", "7"]
+            ];//just one die, not a whole bag yet!
 
 //whole die need to be an object, and then I can take the die color out of the array
 //since each face of a common die will be the same for our purposes so far
 
-function drawAllFaces(dieFaceArray){
+function drawAllFaces(dieFacesArray){
     //Grab a nodelist of all die- elements[attr*=value]
-    var thisDiesFaces = document.querySelectorAll("[id*=div-]");
+    var pageDieLocations = document.querySelectorAll("[id*=die-]");
 
     //iterate through the die face array while senidng to die- div locations
-    for(var i = 0; i < thisDiesFaces.length; i++){
-        
+    for(var i = 0; i < pageDieLocations.length; i++){
+        //send a single face array from the dieFacesArray to a die- div location
+        drawDieToBrowser(dieFacesArray[i], pageDieLocations[i])
     }
 }
 
-function drawDieToBrowser(dieFaceArray, thisFace){
+function drawDieToBrowser(dieFaceArray, dieDiv){
     
     //Set some meaningful variable names
-    var dieBodyColor    = dieFaceArray[thisFace][0];
-    var diePenColor     = dieFaceArray[thisFace][1];
-    var dieFaceDots     = dieFaceArray[thisFace][2];
-    var dieFaceRune     = dieFaceArray[thisFace][3];
-    var dieFaceNumVal   = dieFaceArray[thisFace][4];
+    var dieBodyColor    = dieFaceArray[0];
+    var diePenColor     = dieFaceArray[1];
+    var dieFaceDots     = dieFaceArray[2];
+    var dieFaceRune     = dieFaceArray[3];
+    var dieFaceNumVal   = dieFaceArray[4];
 
     //grab a die div (and set background as dieBodyColor)
-    var thisDieFace = document.getElementById("die-1");
-    thisDieFace.style.background = dieBodyColor;
+    //var thisDieFace = document.getElementById("die-1");
+    var thisDieLocation = dieDiv;
+    thisDieLocation.style.background = dieBodyColor;
 
     //grab the list of three <use> elements from each die- div
-    var dieDrawing = thisDieFace.firstElementChild.children;
+    var dieDrawing = thisDieLocation.firstElementChild.children;
 
     //trudge through and change values based on dieBag vars
     dieDrawing[0].attributes[0].value = `#${dieFaceRune}-bottom`;
